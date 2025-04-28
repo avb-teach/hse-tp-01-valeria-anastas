377c951a212c894dfbc46004686641ce971100ac
Валерия Анастас <vvanastas@edu.hse.ru>
2025-04-28 04:36:47 +0300

    fix task 5

--- collect_files.sh
+++ collect_files.sh
@@ -2,18 +2,31 @@
 
 input_dir=$1
 output_dir=$2
-if [[ $3 == --max_depth && -n $4 && $4 -ge 0 ]]; then
-  extra_args="-maxdepth $4"
+max_depth=1
+if [[ $3 == --max_depth && $4 -ge 1 ]]; then
+  max_depth=$4
 fi
 declare -i count
 
-for f in $(find $input_dir $extra_args -type f); do
+for f in $(find $input_dir -type f); do
   basename=${f##*/}
+  dir=${f%$basename}
+  dir=${dir#$input_dir/}
+  for ((i = 1; i < $max_depth; i++)); do
+    if [[ -n $dir ]]; then
+      dir=${dir%/}
+      p=${dir##*/}
+      basename=$p/$basename
+      dir=${dir%$p}
+    fi
+  done
   name=$basename
   count=0
   while [[ -e $output_dir/$name ]]; do
     count+=1
     name=${basename/./$count.}
   done
-  cp $f $output_dir/$name
+  dest=$output_dir/$name
+  mkdir -p ${dest%${dest##*/}}
+  cp $f $dest
 done
