f306ab351fb613d7c62978a57bc886bc9983a792
Валерия Анастас <vvanastas@edu.hse.ru>
2025-04-28 03:59:55 +0300

    add task 5

--- collect_files.sh
+++ collect_files.sh
@@ -2,10 +2,12 @@
 
 input_dir=$1
 output_dir=$2
-
+if [[ $3 == --max_depth && -n $4 && $4 -ge 0]]; then
+  extra_args="-maxdepth $4"
+fi
 declare -i count
 
-for f in $(find $input_dir -type f); do
+for f in $(find $input_dir $extra_args -type f); do
   basename=${f##*/}
   name=$basename
   count=0
